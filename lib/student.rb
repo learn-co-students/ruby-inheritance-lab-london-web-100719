08a9d1cc4929b13520f658ff8e8859f0c5b1b430
Liko <li_xie1992@hotmail.com>
2019-10-15 12:03:05 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    @@all = [] 
+
+    def initialize
+        @knowledge = []
+        @@all << self
+    end
+
+    def learn (knowledge_string)
+        self.knowledge << knowledge_string
+    end  
+    
+    def self.all
+        @@all
+    end
 
 end
