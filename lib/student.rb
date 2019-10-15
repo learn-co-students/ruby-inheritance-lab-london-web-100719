933e285610db74a770a779684d69ab673914dc47
Peter Sampson <peter.sampson010@gmail.com>
2019-10-15 13:43:57 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end 
+
+    def learn(piece_of_knowledge)
+        @knowledge << piece_of_knowledge
+    end 
+
+    def knowledge
+        @knowledge 
+    end 
+
 
 end
